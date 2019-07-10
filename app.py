88faf1ea7b65c6e5bf1e0aeab5db8060216003c1
playerla <playerla@example.com>
2019-07-11 01:50:30 +0200

    One page app

--- app.py
+++ app.py
@@ -15,22 +15,17 @@ class User(db.Model, View):
     username = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), nullable=False)
     
-    @property
-    def json(self):
-        return { 'username': "toto" }
-
-    @app.route("/add/<username>/<email>")
-    def add(**kwargs):
-        self = User(**kwargs)
-        db.session.add(User(**kwargs))
+    @socketio.on('add')
+    def add(data):
+        new_user = User(**data)
+        db.session.add(new_user)
         db.session.commit()
-        return self.get()
+        User.get()
     
-    @socketio.on('message')
-    def get(self, **kwargs):
+    @app.route("/get")
+    def get():
         users = [{'username': u.username, 'email': u.email } for u in User.query.all()]
-        socketio.send(users)
-        print(users)
+        socketio.emit('message', users)
         return render_template('users.html', users=users)
 
 open("app.db", 'w').close()
@@ -41,14 +36,24 @@ def main():
     return """
 <script src="//cdnjs.cloudflare.com/ajax/libs/socket.io/2.2.0/socket.io.js" integrity="sha256-yr4fRk/GU1ehYJPAs8P4JlTgu0Hdsp4ZKrx8bDEDC3I=" crossorigin="anonymous"></script>
 <script type="module" charset="utf-8">
+    import 'https://unpkg.com/@material/mwc-button@0.6.0/mwc-button.js?module';
     import {html, render} from 'https://unpkg.com/lit-html?module';
+    function add() {
+        socket.emit('add', {
+                            username: document.getElementById('username').value, 
+                            email:    document.getElementById('email').value
+                           })
+    }
     const Users = (users) => html`
         <ul>
             ${users.map((u) => html`<li><strong>${ u.username }</strong> ${ u.email }</li>`)}
-        </ul>`;
+        </ul>
+        <input id='username' value='username'>
+        <input id='email' value='email'>
+        <mwc-button unelevated label="Add" @click="${add}"></mwc-button>`;
+    render(Users([]), document.body);
     var socket = io();
     socket.on('message', function(json){
-        console.log(json);
         render(Users(json), document.body);
     });
 </script>

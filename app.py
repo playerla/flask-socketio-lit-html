0b8f90358c46623ca012db59ab6191f9eb8ea492
playerla <playerla@example.com>
2019-07-12 01:11:46 +0200

    Clean code

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask,render_template, jsonify
+from flask import Flask, render_template
 from flask.views import View
 from flask_sqlalchemy import SQLAlchemy
-from flask_socketio import SocketIO, send
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -15,48 +15,21 @@ class User(db.Model, View):
     username = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), nullable=False)
     
+    def list_users():
+        return [{'username': u.username, 'email': u.email } for u in User.query.all()]
+
     @socketio.on('add')
     def add(data):
         new_user = User(**data)
         db.session.add(new_user)
         db.session.commit()
-        User.get()
+        socketio.emit('message', User.list_users())
     
-    @app.route("/get")
-    def get():
-        users = [{'username': u.username, 'email': u.email } for u in User.query.all()]
-        socketio.emit('message', users)
-        return render_template('users.html', users=users)
-
 open("app.db", 'w').close()
 db.create_all()
 
 @app.route('/')
 def main():
-    return """
-<script src="//cdnjs.cloudflare.com/ajax/libs/socket.io/2.2.0/socket.io.js" integrity="sha256-yr4fRk/GU1ehYJPAs8P4JlTgu0Hdsp4ZKrx8bDEDC3I=" crossorigin="anonymous"></script>
-<script type="module" charset="utf-8">
-    import 'https://unpkg.com/@material/mwc-button@0.6.0/mwc-button.js?module';
-    import {html, render} from 'https://unpkg.com/lit-html?module';
-    function add() {
-        socket.emit('add', {
-                            username: document.getElementById('username').value, 
-                            email:    document.getElementById('email').value
-                           })
-    }
-    const Users = (users) => html`
-        <ul>
-            ${users.map((u) => html`<li><strong>${ u.username }</strong> ${ u.email }</li>`)}
-        </ul>
-        <input id='username' value='username'>
-        <input id='email' value='email'>
-        <mwc-button unelevated label="Add" @click="${add}"></mwc-button>`;
-    render(Users([]), document.body);
-    var socket = io();
-    socket.on('message', function(json){
-        render(Users(json), document.body);
-    });
-</script>
-    """
+    return render_template('main.html')
 
 socketio.run(app)

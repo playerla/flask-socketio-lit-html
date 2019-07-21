7d8a2658923f6ceb1a00409385cd8d5ebfd9d9bc
playerla <playerla@example.com>
2019-07-22 01:39:36 +0200

    Blue print and column introspection First stop to make an abstract IndexModel class Trying to support arbitrary children columns

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, Blueprint
 from flask_sqlalchemy import Model, SQLAlchemy
 import sqlalchemy as sa
 from sqlalchemy.ext.declarative import declared_attr
@@ -13,8 +13,18 @@ app.config['DEBUG'] = 'True'
 socketio = SocketIO(app, engineio_logger=True)
 db = SQLAlchemy(app, session_options={'autocommit': True})
 
+bp_users = Blueprint('users', __name__, template_folder='users_template')
+
 # https://flask-sqlalchemy.palletsprojects.com/en/2.x/customizing/
 class IndexModel(Model):
+    __abstract__ = True
+    
+    @classmethod
+    def register(cls, blueprint):
+        print(cls)
+        blueprint.add_url_rule('/meta', view_func=IndexModel.loop, defaults = { 'cls': cls })
+        blueprint.add_url_rule('/user/<int:index>', view_func=IndexModel.get, defaults = { 'cls': cls })
+        
     @declared_attr
     def index(cls):
         for base in cls.__mro__[1:-1]:
@@ -25,13 +35,16 @@ class IndexModel(Model):
             type = sa.Integer
 
         return sa.Column(type, primary_key=True)
-
-    @classmethod
+    
     def loop(cls):
-        for c in cls.__table__.columns:
-            print(c, type(c))
+        return { i.index : i._asdict() for i in db.session.query(*cls.__table__.columns) }
 
-    ColumnLookupError = Exception("Error during lookup sqlalchemy.sql.schema.Column")
+    def get(cls, index):
+        u = db.session.query(cls).filter(cls.index==index).first()
+        if u:
+            return {'username': u.username, 'email': u.email }
+        else:
+            return "Not found", 404
 
 db = SQLAlchemy(app, model_class=IndexModel)
 
@@ -39,23 +52,15 @@ class User(db.Model):
     username = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), nullable=False)
 
-    @app.route('/user')
+    @bp_users.route('/user')
     def user():
         return render_template('user.js', ioupdate=str(User)+'update'), { 'Content-Type': "text/javascript; charset=utf-8" }
 
-    @app.route('/users')
+    @bp_users.route('/users')
     def list_users():
         return { 'users': db.session.query(User.index).all() } 
 
-    @app.route('/user/<int:index>')
-    def get(index):
-        u = User.query.get(index)
-        if u:
-            return {'username': u.username, 'email': u.email }
-        else:
-            return "Not found", 404 
-
-    @app.route('/user', methods=['POST'])
+    @bp_users.route('/user', methods=['POST'])
     def post():
         u = db.session.merge(User(**request.get_json()))
         db.session.commit()
@@ -69,6 +74,9 @@ db.create_all()
 def all_exception_handler(error):
     return app.send_static_file('404.html'), 404
     
+User.register(bp_users)
+app.register_blueprint(bp_users)
+
 @app.route('/')
 def main():
     return render_template('main.html')

3e329bf93dc8ab9c3a3dacab889efa504f01d9c1
playerla <playerla@example.com>
2019-07-28 18:26:40 +0200

    Item update itself with post Bug: update() called every time

--- webcomponent_templates/webcomponent_base.js
+++ webcomponent_templates/webcomponent_base.js
@@ -29,12 +29,25 @@ class Item extends LitElement {
             },
             {% endfor %}
         }
-    }
+    };
     _set(properties) {
         {% for property in properties %}
-        this.{{ property }} = properties.{{ property }};
+            this.{{ property }} = properties.{{ property }};
         {% endfor %}
+    };
+    newItem(properties) {
+        post('{{ base_url }}', properties).then(json => this.index = json.index);
+        console.log('new', this.index);
         return this;
+        console.log('exec2');
+    };
+    update() {
+        console.log('update!!!!!');
+        post('{{ base_url }}', {
+        {% for property in properties %}
+           {{ property }}: this.{{ property }},
+        {% endfor %}
+        })
     }
     _get() { 
         get('{{ base_url }}'+'/'+this.index).then(item => {
@@ -44,14 +57,14 @@ class Item extends LitElement {
             }
             else
                 console.log("undefined item ", this.index);
-    })}
+    })};
     updated(changedProperties) {
         if (changedProperties.has('index')) {
             console.log(this.index, 'index updated')
             // get the new item referenced by the primary key this.index
             this._get();
         }
-    }
+    };
     constructor() {
         super();
         var element = this // Capturing element in the update callback
@@ -89,41 +102,26 @@ class Items extends LitElement {
     constructor() {
         super();
         this.items = {};
-        var element = this // Capturing element in the update callback
-        io_socket.on("{{ ioupdate }}", function(index_update) {
-            if (element.items[index_update] == undefined)
-                element._posted({index: index_update});
-            get('{{ base_url }}'+'/all').then(json => {
-                // Removing all removed items
-                for (var index in element.items) {
-                    if (!element.items[index].index in json.items) {
-                        element.items[index].remove();
-                        element.items[index] = undefined;
-                    }
-                }
-            });
-        });
     };
     firstUpdated() {
         this.$ul = this.shadowRoot.querySelector('ul');
     }
-    _posted(properties) {
-        var child = document.createElement('{{ component_name }}')._set(properties);
-        this.items[properties.index] = child;
-        var $li = document.createElement('li');
-        $li.appendChild(child)
-        this.$ul.appendChild($li);
-    }
-    _add({index=null}) {
-        post('{{ base_url }}',
-        {
-            index: index, 
-            username: this.shadowRoot.getElementById('username').value, 
+    data() {
+        return {
+           username: this.shadowRoot.getElementById('username').value, 
             email: this.shadowRoot.getElementById('email').value
+        }
+    }
+    add_event() {
+        var child = document.createElement('{{ component_name }}').newItem(this.data()).then( (newItem) => {
+            this.items[newItem.index] = newItem;
+            var $li = document.createElement('li');
+            $li.appendChild(newItem)
+            this.$ul.appendChild($li);
         });
     }
-    _change() { 
-        this._add({index:this.shadowRoot.getElementById('index').value}) ;
+    change_event() { 
+        this.items({index:this.shadowRoot.getElementById('index').value}) ;
     }
     render() {
         return html`
@@ -138,12 +136,12 @@ class Items extends LitElement {
                 <input type="email" class="form-control input-lg" id="email" aria-describedby="emailHelp" value="name@example.com">
                 <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
             </div>
-            <button class="btn btn-primary" @click="${ this._add }">Add</button>
+            <button class="btn btn-primary" @click="${ this.add_event }">Add</button>
         </form>
         <ul>
         </ul>
         <input id='index' value='2'>
-        <mwc-button unelevated label="Change" @click="${ this._change }"></mwc-button>`;
+        <mwc-button unelevated label="Change" @click="${ this.change_event }"></mwc-button>`;
     }
 }
 window.customElements.define('ul-{{ component_name }}', Items);
